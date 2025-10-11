b997c331f0b3de6f2882ee6b68b1b496eb6bdd98
Krish <krishverma837@gmail.com>
2025-10-11 23:39:41 +0530

    formatted the code

--- src/main/java/com/dcl/accommodate/security/jwt/JwtService.java
+++ src/main/java/com/dcl/accommodate/security/jwt/JwtService.java
@@ -24,11 +24,11 @@ public class JwtService {
 
     private final Key key;
 
-    public JwtService(AppEnv env){
+    public JwtService(AppEnv env) {
         this.key = Keys.hmacShaKeyFor(Base64.getDecoder().decode(env.getJwt().getSecret()));
     }
 
-    public String generateAccessToken(Map<String,Object> claims, String subject, Duration ttl){
+    public String generateAccessToken(Map<String, Object> claims, String subject, Duration ttl) {
         var systemMillis = System.currentTimeMillis();
         return Jwts.builder()
                 .setClaims(claims)
@@ -39,18 +39,18 @@ public class JwtService {
                 .compact(); //
     }
 
-    public String generateRefreshToken( String subject, Duration ttl){
+    public String generateRefreshToken(String subject, Duration ttl) {
         var systemMillis = System.currentTimeMillis();
         return Jwts.builder()
                 .setIssuedAt(new Date(systemMillis))
                 .setExpiration(new Date(systemMillis + ttl.toMillis()))
                 .setSubject(subject)
                 .signWith(key, SignatureAlgorithm.HS256)
-                .compact(); //
+                .compact();
     }
 
 
-    private Claims getClaims(String token){
+    private Claims getClaims(String token) {
         return Jwts.parserBuilder()
                 .setSigningKey(key)
                 .build()//return a parser to parse JWT
